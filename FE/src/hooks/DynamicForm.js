e365a5d880f1832903fff62f6ce55cac5c422e06
Chung Nguyen <zrmedia9@gmail.com>
2022-05-26 09:26:59 +0700

    useRef

--- FE/src/hooks/DynamicForm.js
+++ FE/src/hooks/DynamicForm.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import AddIcon from '@material-ui/icons/Add';
 import PropTypes from 'prop-types';
-import Fab from "@material-ui/core/Fab";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
+import Fab from '@material-ui/core/Fab';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   container: {
@@ -17,23 +17,37 @@ const useStyles = makeStyles({
 
 const DynamicForm = ({ labels }) => {
   const [fields, setFields] = useState(labels);
+  const containerRef = useRef(null);
   const classes = useStyles();
 
   const handleAddField = () => {
     setFields([...fields, 'New Field']);
-  }
+  };
+
+  const getHeight = () => {
+    if (containerRef && containerRef.current) {
+      return containerRef.current.offsetHeight;
+    }
+  };
 
   return (
     <>
-      <div className={classes.container}>
-        {fields.map(field => <TextField
-          id={field}
-          label={field}
-          variant="outlined"
-          color="secondary"
-        />)}
+      <div ref={containerRef} className={classes.container}>
+        {fields.map((field) => (
+          <TextField
+            id={field}
+            label={field}
+            variant="outlined"
+            color="secondary"
+          />
+        ))}
       </div>
-      <Fab onClick={handleAddField} color="secondary" aria-label="add">
+      <div>{getHeight()}</div>
+      <Fab
+        onClick={handleAddField}
+        color="secondary"
+        aria-label="add"
+      >
         <AddIcon />
       </Fab>
     </>
